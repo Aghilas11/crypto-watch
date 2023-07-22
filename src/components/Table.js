09d3675dbeec9f49ac3fd77eb5bc52c91f379507
AGHILAS <halouane.aghilas@yahoo.com>
2023-07-22 16:19:14 +0200

    navbar suite

--- src/components/Table.js
+++ src/components/Table.js
@@ -3,21 +3,50 @@ import TableLine from "./TableLine";
 import ToTop from "./ToTop";
 
 const Table = ({ coinsData }) => {
-  const [rangeNumber, setRangeNumber] = useState(100);
   const [orderBy, setOrderBy] = useState("");
+  const [rangeNumber, setRangeNumber] = useState(100);
 
   const tableHeader = [
     "Prix",
-    "MarketCape",
+    "MarketCap",
     "Volume",
     "1h",
     "1j",
     "1s",
     "1m",
     "6m",
-    "1a",
+    "1y",
     "ATH",
   ];
+
+  const excludeCoin = (coin) => {
+    if (
+      coin === "usdt" ||
+      coin === "usdc" ||
+      coin === "busd" ||
+      coin === "dai" ||
+      coin === "ust" ||
+      coin === "mim" ||
+      coin === "tusd" ||
+      coin === "usdp" ||
+      coin === "usdn" ||
+      coin === "fei" ||
+      coin === "tribe" ||
+      coin === "gusd" ||
+      coin === "frax" ||
+      coin === "lusd" ||
+      coin === "husd" ||
+      coin === "ousd" ||
+      coin === "xsgd" ||
+      coin === "usdx" ||
+      coin === "eurs"
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  };
+
   return (
     <div className="table-container">
       <ul className="table-header">
@@ -28,7 +57,7 @@ const Table = ({ coinsData }) => {
               type="text"
               value={rangeNumber}
               onChange={(e) => setRangeNumber(e.target.value)}
-            />{" "}
+            />
           </span>
           <input
             type="range"
@@ -42,9 +71,6 @@ const Table = ({ coinsData }) => {
         {tableHeader.map((el) => (
           <li key={el}>
             <input
-              type="radio"
-              name="header-el"
-              id={el}
               defaultChecked={
                 el === orderBy || el === orderBy + "reverse" ? true : false
               }
@@ -55,6 +81,9 @@ const Table = ({ coinsData }) => {
                   setOrderBy(el);
                 }
               }}
+              type="radio"
+              name="header-el"
+              id={el}
             />
             <label htmlFor={el}>{el}</label>
           </li>
@@ -67,11 +96,88 @@ const Table = ({ coinsData }) => {
             switch (orderBy) {
               case "Prix":
                 return b.current_price - a.current_price;
+              case "Volume":
+                return b.total_volume - a.total_volume;
+              case "MarketCap":
+                return b.market_cap - a.market_cap;
+              case "1h":
+                return (
+                  b.price_change_percentage_1h_in_currency -
+                  a.price_change_percentage_1h_in_currency
+                );
+              case "1j":
+                return (
+                  b.market_cap_change_percentage_24h -
+                  a.market_cap_change_percentage_24h
+                );
+              case "1s":
+                return (
+                  b.price_change_percentage_7d_in_currency -
+                  a.price_change_percentage_7d_in_currency
+                );
+              case "1m":
+                return (
+                  b.price_change_percentage_30d_in_currency -
+                  a.price_change_percentage_30d_in_currency
+                );
+              case "6m":
+                return (
+                  b.price_change_percentage_200d_in_currency -
+                  a.price_change_percentage_200d_in_currency
+                );
+              case "1a":
+                return (
+                  b.price_change_percentage_1y_in_currency -
+                  a.price_change_percentage_1y_in_currency
+                );
+              case "ATH":
+                return b.ath_change_percentage - a.ath_change_percentage;
+              case "#reverse":
+                return a.market_cap - b.market_cap;
               case "Prixreverse":
                 return a.current_price - b.current_price;
+              case "Volumereverse":
+                return a.total_volume - b.total_volume;
+              case "MarketCapreverse":
+                return a.market_cap - b.market_cap;
+              case "1hreverse":
+                return (
+                  a.price_change_percentage_1h_in_currency -
+                  b.price_change_percentage_1h_in_currency
+                );
+              case "1jreverse":
+                return (
+                  a.market_cap_change_percentage_24h -
+                  b.market_cap_change_percentage_24h
+                );
+              case "1sreverse":
+                return (
+                  a.price_change_percentage_7d_in_currency -
+                  b.price_change_percentage_7d_in_currency
+                );
+              case "1mreverse":
+                return (
+                  a.price_change_percentage_30d_in_currency -
+                  b.price_change_percentage_30d_in_currency
+                );
+              case "6mreverse":
+                return (
+                  a.price_change_percentage_200d_in_currency -
+                  b.price_change_percentage_200d_in_currency
+                );
+              case "1areverse":
+                return (
+                  a.price_change_percentage_1y_in_currency -
+                  b.price_change_percentage_1y_in_currency
+                );
+              case "ATHreverse":
+                return a.ath_change_percentage - b.ath_change_percentage;
+              default:
             }
           })
-          .map((coin, index) => <TableLine coin={coin} index={index} />)}
+          .map((coin, index) => (
+            <TableLine coin={coin} key={coin.id} index={index} />
+          ))}
     </div>
   );
 };
